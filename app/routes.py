d28da70e66b9164185ba10e335b2a83f21b1dbfa
Daniel Soutner <dan@soutner.cz>
2021-10-12 18:58:03 +0200

    fix typo

--- app/routes.py
+++ app/routes.py
@@ -21,4 +21,4 @@ def annotation():
     with open("data", "a") as fout:
         fout.write(f"{idx} {value}\n")
 
-    return f"<h1>Dky</h1>Pro značku {idx} uloženo {value}"
+    return f"<h1>Díky</h1>Pro značku {idx} uloženo {value}"
