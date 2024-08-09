c2e01ea07f88767d2bf43c752fc0818e97b3f9a9
dan <dan@soutner.cz>
2024-08-09 14:40:19 +0200

    add all time map

---
+++ download_and_render.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set pipefail -e
+
+LOGLEVEL=INFO
+
+# Download the data
+python run_db_update.py --filter_cyklo --db_json plznito_cyklo.json
+python run_db_update.py --db_json plznito_all.json
+
+# Render the data
+python run_map_render.py --file_in plznito_cyklo.json --file_out app/templates/map.html
+python run_map_render.py --file_in plznito_all.json --file_out app/templates/map_all.html
