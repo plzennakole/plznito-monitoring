088857867acd02417bed10d254fd0fad39306e10
dan <dan@soutner.cz>
2024-08-09 15:26:29 +0200

    speed up the map

--- download_and_render.sh
+++ download_and_render.sh
@@ -10,5 +10,5 @@ python run_db_update.py --db_json plznito_all.json
 
 # Render the data
 python run_map_render.py --file_in plznito_cyklo.json --file_out app/templates/map.html
-python run_map_render.py --cluster-style --file_in plznito_all.json --file_out app/templates/map_all.html
+python run_map_render.py --cluster_style --file_in plznito_all.json --file_out app/templates/map_all.html
 
